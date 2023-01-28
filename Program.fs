313d5fec9b36c1c29361779deb432e6b62ad749c
elton.cassas <elcassastn50@gmail.com>
2023-01-27 22:42:01 -0400

    initial commit

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+let promptUser () = 
+    Console.WriteLine("(d)eposit, (w)ithdraw or e(x)it: ")
+    Console.ReadLine()
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Hello from the transaction processor!")
+
+    let mutable running = true
+    while running do
+        let action = promptUser()
+        printfn "You told me to do this: %A" action
+
+        running <- action <> "x"
+    
+    Console.WriteLine("Bye!")
+    0
